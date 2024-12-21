51f4d2a1617fb58687749fda79d0b6aa755ea27e
j-bellemare <jathanbellemare@gmail.com>
2024-12-21 14:06:03 -0500

    updating DB, settings, admin, and requirments

--- api_handler_app/admin.py
+++ api_handler_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Players, GameData
 
-# Register your models here.
+
+class PlayersAdmin(admin.ModelAdmin):
+    list_display = ("puuid", "name", "tag", "rank")
+
+
+class GameDataAdmin(admin.ModelAdmin):
+    list_display = ("id", "game_id", "game_mode", "player", "player_data")
+
+
+admin.site.register(Players, PlayersAdmin)
+admin.site.register(GameData, GameDataAdmin)
