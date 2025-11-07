9279eb79d203c7babc3861dd26a978e5b53bfe04
j-bellemare <jathanbellemare@gmail.com>
2025-11-06 23:40:39 -0500

    that good good

--- riot_api_app/urls.py
+++ riot_api_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import re_path
-from riot_api_app.views import get_matches
+from riot_api_app.views import get_matches, get_match_data, get_player_score, get_teams
 
 urlpatterns = [
-    re_path(r"^api/riot_api_app/", get_matches, name="get_matches")
+    re_path(r"^api/riot_api_app/get_matches", get_matches, name="get_matches"),
+    re_path(r"^api/riot_api_app/get_match_data", get_match_data, name="get_match_data"),
+    re_path(r"^api/riot_api_app/get_player_score", get_player_score, name="get_player_score"),
+    re_path(r"^api/riot_api_app/get_teams", get_teams, name="get_teams"),
 ]
